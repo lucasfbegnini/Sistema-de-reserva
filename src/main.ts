b2bab64d28c9111221ca58d175ac9e22c5d77db4
Quitacomluquitas <lucasfbs.lf@gmail.com>
2025-11-17 15:33:40 -0300

    Admin Master

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const PORT = process.env.PORT || 8000;
+  const PORT = process.env.PORT || 3000;
 
   await app.listen(PORT);
 
